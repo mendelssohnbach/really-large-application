dd1371363470e35ef701b520d21e7e8ca00e0e2f
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-25 06:05:57 +0900

    Update unit test case

--- index.test.js
+++ index.test.js
@@ -14,5 +14,25 @@ describe('統合テスト', function () {
     // 実際値, 期待値
     // 等価である
     assert.strictEqual(todos.list().length, 0);
+
+    // 最初のtodoを登録
+    todos.add('コードの実行');
+    assert.strictEqual(todos.list().length, 1);
+    // 期待されるオブジェクトと実際のオブジェクトが同値プロパティを持っているか？
+    assert.deepStrictEqual(todos.list(), [{ title: 'コードの実行', completed: false }]);
+
+    // 2つ目のtodo
+    todos.add('すべてをテストする');
+    assert.strictEqual(todos.list().length, 2);
+    assert.deepStrictEqual(todos.list(), [
+      { title: 'コードの実行', completed: false },
+      { title: 'すべてをテストする', completed: false },
+    ]);
+
+    todos.complete('コードの実行');
+    assert.deepStrictEqual(todos.list(), [
+      { title: 'コードの実行', completed: true },
+      { title: 'すべてをテストする', completed: false },
+    ]);
   });
 });
