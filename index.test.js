60c3e8f47600e4e3182a60cf91835ff4e3c0764f
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-25 05:47:31 +0900

    unit test 最初のテストのあるべきコード

--- index.test.js
+++ index.test.js
@@ -7,17 +7,12 @@ import assert from 'assert';
 // 統合テスト用のグループを作成
 describe('統合テスト', function () {
   // テストの名前をわかりやすく説明
-  it('TODOsを追加して完了することができるはずです', function () {
+  it('TODOsに最初はアイテムは存在しないはずです', function () {
     // オブジェクトをインスタンス化
     let todos = new Todos();
     // assertモジュールを使用
     // 実際値, 期待値
-    assert.notStrictEqual(todos.list().length, 1);
-  });
-  it('TODOがまったくない場合にのみ完了すること', function () {
-    let todos = new Todos();
-    todos.add('ベッドから起き上がる');
-    todos.add('ベッドを整える');
+    // 等価である
     assert.strictEqual(todos.list().length, 0);
   });
 });
