73b3ce129f91ccca77739a28354ffbf129e2c539
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-27 09:47:51 +0900

    Chapter13 step2

--- requests/README.md
+++ requests/README.md
@@ -122,4 +122,61 @@ Retrieved all data
 ]
 ```
 
+## Step2 request() Options
 
+request（）オプション
+
+```js
+https.request(Options_Object, Callback_Function) {
+    Action
+}
+```
+
+```js
+import https from 'https';
+
+const options = {
+  host: 'jsonplaceholder.typicode.com',
+  path: '/users?_limit=2',
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+  },
+};
+
+const request = https.request(options, (res) => {
+  if (res.statusCode !== 200) {
+    console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
+    res.resume();
+    return;
+  }
+
+  let data = '';
+
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  res.on('close', () => {
+    console.log('Retrieved all data');
+    console.log(JSON.parse(data));
+  });
+});
+
+request.end();
+```
+
+```shell
+$ node getRequestWithRequest.js
+Retrieved all data
+[
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    ...
+      catchPhrase: 'Proactive didactic contingency',
+      bs: 'synergize scalable supply-chains'
+    }
+  }
+]
+```
--- requests/getRequestWithRequest.js
+++ requests/getRequestWithRequest.js
@@ -1,30 +1,31 @@
 import https from 'https';
 
 const options = {
+  host: 'jsonplaceholder.typicode.com',
+  path: '/users?_limit=2',
   method: 'GET',
+  headers: {
+    Accept: 'application/json',
+  },
 };
 
-const request = https.request(
-  'https://jsonplaceholder.typicode.com/users?_limit=2',
-  options,
-  (res) => {
-    if (res.statusCode !== 200) {
-      console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
-      res.resume();
-      return;
-    }
-
-    let data = '';
-
-    res.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    res.on('close', () => {
-      console.log('Retrieved all data');
-      console.log(JSON.parse(data));
-    });
+const request = https.request(options, (res) => {
+  if (res.statusCode !== 200) {
+    console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
+    res.resume();
+    return;
   }
-);
+
+  let data = '';
+
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  res.on('close', () => {
+    console.log('Retrieved all data');
+    console.log(JSON.parse(data));
+  });
+});
 
 request.end();
